b16ba1c40c845e528fb3e4dc143d0cb6a34f6514
yadavanuj44 <95302917+yadavanuj44@users.noreply.github.com>
2021-12-09 00:38:40 +0530

    updated ReactJS code and CSS design

--- src/App.js
+++ src/App.js
@@ -2,37 +2,86 @@ import React, { useState } from "react";
 import "./styles.css";
 
 var emojiDictionary = {
-  "🏁": "Chequered Flag",
-  "🚩": "Triangular Flag",
-  "🎌": "Crossed Flags",
-  "🏴": "Black Flag",
-  "🇦🇫": ""
+  "🧗": "Person Climbing",
+  "🤺": "Person Fencing",
+  "🏇": "Horse Racing",
+  "🏂": "Snowboarding",
+  "🏌️": "Person Golfing",
+  "🏄": "Person Surfing",
+  "🚣": "Person Rowing Boat",
+  "🏊": "Person Swimming",
+  "⛹️": "Person Bouncing Ball",
+  "🏋️": "Person Lifting Weights",
+  "🚴": "Person Biking",
+  "🤸": "Person Cartwheeling",
+  "🤼": "People Wrestling",
+  "🤾": "Person Playing Handball",
+  "🤹": "Person Juggling",
+  "🏆": "Trophy",
+  "⚽": "Soccer Ball",
+  "⚾": "Baseball",
+  "🥏": "Frisbee",
+  "🎳": "Bowling"
 };
 
 export default function App() {
   const [emojiMeaning, setEmojiMeaning] = useState("");
   var emojis = Object.keys(emojiDictionary);
 
-  function emojiMeaningHandler() {}
+  function emojiMeaningHandler(event) {
+    var userInput = event.target.value;
+    var emojiMeaning = emojiDictionary[userInput];
+    if (emojiMeaning === undefined) {
+      emojiMeaning = "This emoji is not defined in our database";
+    }
+    setEmojiMeaning(emojiMeaning);
+  }
 
-  function emojiClickHandler(emoji) {}
+  function emojiClickHandler(emoji) {
+    var emojiMean = emojiDictionary[emoji];
+    setEmojiMeaning(emojiMean);
+  }
 
   return (
     <div className="App">
-      <h1>Welcome to Activity and Sports Emoji Interpretor</h1>
-      <input onChange={emojiMeaningHandler}></input>
+      <h1
+        style={{
+          maxWidth: "60%",
+          display: "block",
+          alignContent: "center",
+          margin: "2rem auto"
+        }}
+      >
+        Welcome to Activity and Sports Emoji Interpretor
+      </h1>
+      <input
+        placeholder="Enter the Activity emoji you wanna know meaning of"
+        onChange={emojiMeaningHandler}
+      ></input>
+      <div style={{ fontSize: "2rem", padding: "0.5rem" }}>{emojiMeaning}</div>
       <h2>Activity emojis we know</h2>
-      {emojis.map(function (item) {
-        return (
-          <span
-            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
-            key={item}
-            onClick={() => emojiClickHandler(item)}
-          >
-            {item}
-          </span>
-        );
-      })}
+      <div
+        style={{
+          maxWidth: "50%",
+          display: "block",
+          alignContent: "center",
+          margin: "auto"
+        }}
+      >
+        {emojis.map(function (item) {
+          return (
+            <span
+              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+              key={item}
+              onClick={() => emojiClickHandler(item)}
+            >
+              {item}
+            </span>
+          );
+        })}
+      </div>
+
+      <div></div>
     </div>
   );
 }
